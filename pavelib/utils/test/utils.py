d7cd0b81a4ed4b90b21891a9150294d80f19cee0
Liubov Fomicheva <liubov@lektorium.tv>
2015-02-04 22:31:59 +0300

    Bokchoy fixes

--- pavelib/utils/test/utils.py
+++ pavelib/utils/test/utils.py
@@ -59,7 +59,7 @@ def check_firefox_version():
     """
     Check that firefox is the correct version.
     """
-    expected_firefox_ver = "Mozilla Firefox 28.0"
+    expected_firefox_ver = "Mozilla Firefox 35.0.1"
     firefox_ver = subprocess.check_output("firefox --version", shell=True).strip()
 
     if firefox_ver != expected_firefox_ver:
