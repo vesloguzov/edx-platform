fe8252e6cb3b069d015685371a8fd1cc143f30e7
Liubov Fomicheva <liubov@lektorium.tv>
2015-03-20 13:41:40 +0300

    Course enrollment email: text changed, enabled by default for devstack

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -30,6 +30,7 @@ for pkg_name in ['track.contexts', 'track.middleware', 'dd.dogapi']:
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 FEATURES['ENABLE_INSTRUCTOR_EMAIL'] = True     # Enable email for all Studio courses
 FEATURES['REQUIRE_COURSE_EMAIL_AUTH'] = False  # Give all courses email (don't require django-admin perms)
+FEATURES['SEND_ENROLLMENT_EMAIL'] = True       # Enable enrollment email
 
 
 ########################## ANALYTICS TESTING ########################
