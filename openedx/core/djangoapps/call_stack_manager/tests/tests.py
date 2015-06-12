4bf597e8049e76aed09d2ec1f65586fd19d1dd1c
utkjad <utkarsh@edx.org>
2015-06-12 04:11:58 +0000

    test fixup

--- openedx/core/djangoapps/call_stack_manager/tests/tests.py
+++ openedx/core/djangoapps/call_stack_manager/tests/tests.py
@@ -15,11 +15,11 @@ class TestingCallStackManager(TestCase):
         """ tests save functionality of call stack manager/ applies same for delete()
         1. classes with CallStackMixin should participate in logging.
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelMixin(id_field=1).save()
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class = latest_log[latest_log.find('<'):]
 
             # desired latest class here
@@ -31,22 +31,22 @@ class TestingCallStackManager(TestCase):
         """tests save functionality of call stack manager / applies same for delete()
         1. classes without CallStackMixin should not participate in logging
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelAnotherCSM(id_field=1).save()
 
-            self.assertEqual(len(l.records), 0)
+            self.assertEqual(len(log_capt.records), 0)
 
     def test_queryset(self):
         """ Tests for Overriding QuerySet API
         1. Tests if classes with CallStackManager gets logged.
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelMixinCSM(id_field=1).save()
 
             ModelMixinCSM.objects.all()
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class = latest_log[latest_log.find('<'):]
 
             # desired latest class here
@@ -58,32 +58,32 @@ class TestingCallStackManager(TestCase):
         """ Tests for Overriding QuerySet API
         1. Tests if classes without CallStackManager does not log
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             # create and save objects of class not overriding queryset API
             ModelNothing(id_field=1).save()
 
             # class not using Manager, should not get logged
             ModelNothing.objects.all()
 
-            self.assertEqual(len(l.records), 0, msg="class does not override Manager, hence should not log anything")
+            self.assertEqual(len(log_capt.records), 0, msg="class does not override Manager, hence should not log anything")
 
     def test_donottrack(self):
         """ Test for @donottrack
         1. calls in decorated function should not get tracked
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             donottrack_func()
-            self.assertEqual(len(l.records), 0, msg="Check @donottrack. should not log anything here!")
+            self.assertEqual(len(log_capt.records), 0, msg="Check @donottrack. should not log anything here!")
 
     def test_parameterized_donottrack(self):
         """ Test for parameterized @donottrack
         1. Should not log calls of classes specified in the decorator @donotrack
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             donottrack_parent_func()
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class = latest_log[latest_log.find('<'):]
 
             # desired latest class here
@@ -95,17 +95,17 @@ class TestingCallStackManager(TestCase):
         """ Tests parameterized nested @donottrack
         1. should not track call of classes specified in decorated with scope bounded to the respective class
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             #  class with CallStackManager as Manager
             ModelAnotherCSM(id_field=1).save()
 
             donottrack_parent_func()
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class1 = latest_log[latest_log.find('<'):]
 
-            latest_log = l.records[-2].getMessage()[:l.records[-2].getMessage().find(':')]
+            latest_log = log_capt.records[-2].getMessage()[:log_capt.records[-2].getMessage().find(':')]
             latest_class2 = latest_log[latest_log.find('<'):]
 
             actual_sequence = [latest_class2, latest_class1]
@@ -116,7 +116,7 @@ class TestingCallStackManager(TestCase):
         """ Tests parameterized nested @donottrack
         1. should not track call of classes specified in decorated with scope bounded to the respective class
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
 
             donottrack_child_func()
 
@@ -125,10 +125,10 @@ class TestingCallStackManager(TestCase):
             ModelAnotherCSM.objects.filter(id_field=1)
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class1 = latest_log[latest_log.find('<'):]
 
-            latest_log = l.records[-2].getMessage()[:l.records[-2].getMessage().find(':')]
+            latest_log = log_capt.records[-2].getMessage()[:log_capt.records[-2].getMessage().find(':')]
             latest_class2 = latest_log[latest_log.find('<'):]
 
             actual_sequence = [latest_class2, latest_class1]
@@ -138,38 +138,38 @@ class TestingCallStackManager(TestCase):
     def test_donottrack_called_in_func(self):
         """ test for function which calls decorated function.
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelAnotherCSM(id_field=1).save()
             ModelMixinCSM(id_field=1).save()
 
             track_it()
 
-            self.assertEqual(len(l.records), 4)
+            self.assertEqual(len(log_capt.records), 4)
 
     def test_donottrack_child_too(self):
         """
         1. subclass should be tracked when superclass is called in a @donottrack decorated function
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelWithCSM(id_field=1).save()
             ModelWithCSMChild(id_field=1, id1_field=1).save()
 
             abstract_do_not_track()
 
-            self.assertEqual(len(l.records), 0)
+            self.assertEqual(len(log_capt.records), 0)
 
     def test_dotrack_parent(self):
         """
         1. subclass should be tracked when superclass is called in a @donottrack decorated function
         """
-        with LogCapture() as l:
+        with LogCapture() as log_capt:
             ModelWithCSM(id_field=1).save()
             ModelWithCSMChild(id_field=1, id1_field=1).save()
 
             abstract_do_track()
 
             # Example - logging new call stack for openedx.core.djangoapps.call_stack_manager.tests.models.modelmixin
-            latest_log = l.records[-1].getMessage()[:l.records[-1].getMessage().find(':')]
+            latest_log = log_capt.records[-1].getMessage()[:log_capt.records[-1].getMessage().find(':')]
             latest_class = latest_log[latest_log.find('<'):]
 
             # desired latest class here
@@ -183,7 +183,7 @@ def abstract_do_track():
     """ Function for inheritence
     """
     ModelWithCSM.objects.filter(id_field=1)
-    ModelWithCSMChild.objects.filter(id1_field1=1)
+    ModelWithCSMChild.objects.filter(id1_field=1)
 
 
 @donottrack(ModelWithCSM)
