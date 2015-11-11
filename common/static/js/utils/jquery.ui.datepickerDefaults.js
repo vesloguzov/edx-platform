6a102c1ea9a90e58ac336a418a032e9460d60a47
Liubov Fomicheva <liubov@lektorium.tv>
2015-11-11 18:43:58 +0300

    JavaScript tests: require configuration for testing fixed

--- common/static/js/utils/jquery.ui.datepickerDefaults.js
+++ common/static/js/utils/jquery.ui.datepickerDefaults.js
@@ -7,7 +7,8 @@ define(["module", "jquery", "jquery.ui"], function(module, $, ui) {
         '%b': 'M', //short month name, locale-aware
         '%B': 'MM' // full month name, locale-aware
     }
-    var dateFormat = module.config().dateFormatPython;
+    // get python date format from default require config, alt format specified for tests
+    var dateFormat = module.config().dateFormatPython || '%Y-%m-%d';
     for (var directive in format_translation){
         dateFormat = dateFormat.replace(directive, format_translation[directive]);
     }
