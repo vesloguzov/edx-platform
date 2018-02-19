e731a82a60d4d11b2dbc15ddf857a7d3ac149663
Liubov Fomicheva <liubov@lektorium.tv>
2018-02-19 20:17:49 +0300

    Certificate issue date format: solution with strftime_localized implemented, conf/locale cleanup

--- conf/locale/en/formats.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Overrides and additional values for english edx-platform formats
-"""
-from __future__ import unicode_literals
-
-CERTIFICATE_DATE_FORMAT = 'F j, Y'
--- conf/locale/ru/formats.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Overrides and additional values for russian edx-platform formats
-"""
-from __future__ import unicode_literals
-
-CERTIFICATE_DATE_FORMAT = 'j E Y года'
